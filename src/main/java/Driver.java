fdb94a433c1c36acaa9b09e48b7e492caba08fe5
Zach Connelly <zmconnelly@gmail.com>
2017-11-29 12:08:02 -0700

    Add std dev to actor deaths analysis

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -20,7 +20,7 @@ public class Driver {
 
     public static void main(String[] args) {
         QueryEngine queryEngine = new QueryEngine();
-        performanceOfActorPairs(queryEngine);
+        actorDiedBeforeMovieRelease(queryEngine);
         queryEngine.closeConnection();
     }
 
